794ce581ee62ff8bc2b154dc97c564cd2b680ad8
debsouryadatta <debsouryadatta@gmail.com>
2025-05-20 23:52:08 +0530

    complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,139 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Trash, Pencil } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export default function page() {
+  const LIMIT = 5;
+  const [page, setPage] = useState(1);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
+  const [editedTodoTitle, setEditedTodoTitle] = useState("");
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${LIMIT}`
+        );
+        setTodos(response.data);
+      } catch (error) {
+        console.log("Error fetching todos", error);
+      }
+    };
+    fetchTodos();
+  }, []);
+
+  const handleCreateNewTodo = () => {
+    if (newTodo.trim() === "") return;
+
+    const newTodoObject = {
+      userId: 1,
+      id: Math.floor(Math.random() * 1000000),
+      title: newTodo,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodoObject]);
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    const filteredTodos = todos.filter((todo: Todo) => todo.id !== id);
+    setTodos(filteredTodos);
+  };
+
+  const startEditingTodo = (id: number) => {
+    setIsEditing(true);
+    setEditingTodoId(id);
+    setEditedTodoTitle(todos.find((todo) => todo.id === id)?.title || "");
+  };
+
+  const handleEditTodo = () => {
+    const updatedTodos = todos.map((todo: Todo) => {
+      if(todo.id === editingTodoId) {
+        return {
+          ...todo,
+          title: editedTodoTitle,
+        }
+      } else {
+        return todo;
+      }
+    })
+    setTodos(updatedTodos);
+    setIsEditing(false);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-10">Todos</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <Input
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        type="text"
+        placeholder="Create a new todo"
+        className="w-[50vw]"
+      />
+      <Button onClick={handleCreateNewTodo}>Create Todo</Button>
+      <div className="mt-10">
+        {todos.length > 0 &&
+          todos.map((todo: Todo) => {
+            return (
+              <div key={todo.id} className="flex items-center p-2">
+                <Checkbox id={todo.id.toString()} className="border-gray-500" />
+                <label htmlFor={todo.id.toString()} className="ml-2">
+                  {todo.title}
+                </label>
+                <Button
+                  onClick={() => handleDeleteTodo(todo.id)}
+                  className="w-6 h-6 ml-2 cursor-pointer"
+                >
+                  <Trash />
+                </Button>
+                <Button
+                  onClick={() => startEditingTodo(todo.id)}
+                  className="w-6 h-6 ml-2 cursor-pointer"
+                >
+                  <Pencil />
+                </Button>
+              </div>
+            );
+          })}
+      </div>
+      {isEditing && (
+        <Dialog open={isEditing} onOpenChange={setIsEditing}>
+          <DialogContent>
+            <Input
+              type="text"
+              value={editedTodoTitle}
+              placeholder="Edit Todo"
+              onChange={(e) => setEditedTodoTitle(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <Button onClick={handleEditTodo}>Done</Button>
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   );
 }
